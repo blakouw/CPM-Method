6cfd86b531d40ec609bfc7a0bca776183e519515
blakouw <ckamillopx@gmail.com>
2023-03-12 21:17:59 +0100

    front poczatek

--- src/App.java
+++ src/App.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class App {
+import javax.swing.*;
+
+public class App extends JFrame {
+    private JPanel CPM;
+    private JButton criticalPathButton;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton deleteNodeButton;
+    private JButton addNodeButton;
+
+    public App(){
+        setTitle("CPM");
+        setSize(600,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
